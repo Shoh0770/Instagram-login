5686c7de78e19d3995ca23d12f2249cbacb83bfb
Ziloliddin <Zilolidddinkamolov@gmail.com>
2022-07-14 20:31:17 +0500

    Initial Commit

---
+++ Instagram login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram login
+//
+//  Created by Macbook Pro 14 Inch on 7/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
